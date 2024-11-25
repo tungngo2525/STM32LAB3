4a37a402bbf2793413f9f5b1879314fb317868a3
tungngo2525 <tungngo2525@gmail.com>
2024-11-25 23:43:18 +0700

    init for ex4

---
+++ LAB3/Core/Inc/software_timer.h
@@ -0,0 +1,22 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Nov 25, 2024
+ *      Author: tungn
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+#include "main.h"
+
+#define TIMER_NUMBER 4
+#define TIMER_DURATION 10
+
+extern uint8_t timerFlags[TIMER_NUMBER];
+
+void timerSet(int duration, int index);
+void timerRun(void);
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
+
