4e111b2ce0323ac6ad6510d00c1ee7775ce332e4
tungngo2525 <tungngo2525@gmail.com>
2024-11-26 20:16:01 +0700

    init for EX5

--- LAB3/Debug/Core/Src/input_button.d
+++ LAB3/Debug/Core/Src/input_button.d
@@ -1,4 +1,5 @@
-Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
+Core/Src/input_button.o: ../Core/Src/input_button.c \
+ ../Core/Inc/input_button.h ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
  ../Core/Inc/stm32f1xx_hal_conf.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
@@ -23,7 +24,9 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
  ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
- ../Core/Inc/software_timer.h ../Core/Inc/main.h
+ ../Core/Inc/software_timer.h
+
+../Core/Inc/input_button.h:
 
 ../Core/Inc/main.h:
 
@@ -76,5 +79,3 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
 
 ../Core/Inc/software_timer.h:
-
-../Core/Inc/main.h:
